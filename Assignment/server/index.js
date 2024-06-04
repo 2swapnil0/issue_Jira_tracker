f11004347538589d35e3d997af6e59848491e2d0
Swapnil Mhatre <91521498+2swapnil0@users.noreply.github.com>
2024-06-04 23:23:53 +0530

    Update index.js

--- Assignment/server/index.js
+++ Assignment/server/index.js
@@ -5,9 +5,12 @@ import cors from 'cors';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 8000;
 
-const { JIRA_BASE_URL, JIRA_API_TOKEN, JIRA_PROJECT_KEY } = process.env;
+const JIRA_BASE_URL='https://swapnilmhatre.atlassian.net/'
+const JIRA_API_TOKEN='ATATT3xFfGF0urGDkkqWbjuuIHlLuVxxcc_S_NS9DILFSa9aj94QuYdbpAGqNWz_TuoeYx59knlCoibZR5gEz3bXLgnh2_iIgHYakcei3MAJ86ZFOmPX7kVzFhCosvu9s2wjIQfavDCDLRQLVrJGQsaL1EZ2mryP_InZjaj5oDDacSSUX0VF_0A=972A81F5'
+const JIRA_PROJECT_KEY='KAN'
+
 
 const headers = {
   'Authorization': `Basic ${Buffer.from(`swapnilmhatre671@gmail.com:${JIRA_API_TOKEN}`).toString('base64')}`,
@@ -32,6 +35,7 @@ app.get('/issues', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
 
 
 

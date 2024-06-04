eeca1a81fa36cfc5213b3b2bee62f5f8d5126418
Swapnil Mhatre <91521498+2swapnil0@users.noreply.github.com>
2024-06-04 23:34:26 +0530

    Update index.js

--- Assignment/server/index.js
+++ Assignment/server/index.js
@@ -5,10 +5,10 @@ import cors from 'cors';
 const app = express();
 const PORT = 8000;
 
-const JIRA_BASE_URL='https://swapnilmhatre.atlassian.net/'
-const JIRA_API_TOKEN='ATATT3xFfGF0urGDkkqWbjuuIHlLuVxxcc_S_NS9DILFSa9aj94QuYdbpAGqNWz_TuoeYx59knlCoibZR5gEz3bXLgnh2_iIgHYakcei3MAJ86ZFOmPX7kVzFhCosvu9s2wjIQfavDCDLRQLVrJGQsaL1EZ2mryP_InZjaj5oDDacSSUX0VF_0A=972A81F5'
-const JIRA_PROJECT_KEY='KAN'
-
+// JIRA Configuration
+const JIRA_BASE_URL = 'https://swapnilmhatre.atlassian.net/';
+const JIRA_API_TOKEN = 'ATATT3xFfGF0urGDkkqWbjuuIHlLuVxxcc_S_NS9DILFSa9aj94QuYdbpAGqNWz_TuoeYx59knlCoibZR5gEz3bXLgnh2_iIgHYakcei3MAJ86ZFOmPX7kVzFhCosvu9s2wjIQfavDCDLRQLVrJGQsaL1EZ2mryP_InZjaj5oDDacSSUX0VF_0A=972A81F5';
+const JIRA_PROJECT_KEY = 'KAN';
 
 const headers = {
   'Authorization': `Basic ${Buffer.from(`swapnilmhatre671@gmail.com:${JIRA_API_TOKEN}`).toString('base64')}`,
@@ -33,9 +33,6 @@ app.get('/issues', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

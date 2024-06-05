2805445c9ddb2e2bb39b0d2bb9f92758dcbeea79
Swapnil Mhatre <91521498+2swapnil0@users.noreply.github.com>
2024-06-05 08:52:52 +0530

    Update index.js

--- Assignment/server/index.js
+++ Assignment/server/index.js
@@ -3,12 +3,9 @@ import axios from 'axios';
 import cors from 'cors';
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-const JIRA_BASE_URL = process.env.JIRA_BASE_URL || 'https://swapnilmhatre.atlassian.net';
-const JIRA_API_TOKEN = process.env.JIRA_API_TOKEN || 'ATATT3xFfGF04iBGDA1mZgqqN1WPm_3BCu83KsN2nCjOdNGa5Pf2aAESeZPd4zr-WaNbTAnlO9nNq5buTRr2pi-r6ehy2EXRlZ7pRVzyB_x9ngnU0fyJOzGpZwYCImcFDWi5oxudayMu8LBWYsH2LjPPW7OB87PDdYTL0nNU5GzSWOezlvx-fhM=2094F9BB';
-const JIRA_PROJECT_KEY = process.env.JIRA_PROJECT_KEY || 'KAN';
+const { JIRA_BASE_URL, JIRA_API_TOKEN, JIRA_PROJECT_KEY } = process.env;
 
 const headers = {
   'Authorization': `Basic ${Buffer.from(`swapnilmhatre671@gmail.com:${JIRA_API_TOKEN}`).toString('base64')}`,
